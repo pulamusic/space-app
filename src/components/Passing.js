877dc33bb60878dc06f4e4920c28322d582955f1
Jim Carroll <jim@pulamusic.com>
2020-11-20 14:44:35 -0500

    changed one word

--- src/components/Passing.js
+++ src/components/Passing.js
@@ -9,7 +9,7 @@ export default function Passing({ data }) {
     <p key={i}>
       Misses {_.orbiting_body} tomorrow at{' '}
       {format(_.epoch_date_close_approach, 'h:mmaaaa')} by{' '}
-      {formatter(parseInt(_.miss_distance.miles, 10))} miles whilst travelling
+      {formatter(parseInt(_.miss_distance.miles, 10))} miles while travelling
       at {formatNumber({ truncate: 0 })(_.relative_velocity.miles_per_hour)}mph
     </p>
   ));
